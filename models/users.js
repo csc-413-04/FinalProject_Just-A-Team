8f6def13aae3709facd6d408013c1ab981f4f19a
Ganzolboo Ayurzana <a.ganzolboo@yahoo.com>
2018-12-19 19:31:56 -0800

    FInal

---
+++ models/users.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ganzolboo and Minho on 12.12.2018.
+ */
--- src/redux/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import testReducer from './testReducer';
-//import userReducer from './userReducer';
-
-export default combineReducers({ testReducer });
